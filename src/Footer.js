553a2120674ca9baa9419cd8a3276a6a0a0ace6c
Vicente Plata <vplata@forhims.com>
2019-10-11 22:52:18 -0400

    😶 Remove credit of deleted background

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,9 @@
 import React, { memo } from 'react';
-import UnsplashCredit from './UnsplashCredit';
 
 export default memo(function Footer() {
   return (
     <div>
       <sub>&copy; {1900 + new Date().getYear()} Vicente Plata</sub>
-      <UnsplashCredit />
     </div>
   )
 });
