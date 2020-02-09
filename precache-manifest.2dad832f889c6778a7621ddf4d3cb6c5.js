d49f9d17122b68bf2ca49b57f9fa2b570e958f29
Vicente Plata <vplata@forhims.com>
2020-02-09 12:53:46 -0500

    Updates

---
+++ precache-manifest.2dad832f889c6778a7621ddf4d3cb6c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc085dd2c1da14acbe5bff7f1effa1c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f0fc8bf31a709d61f9",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3fffe857cbe698e7f30c",
+    "url": "/static/css/main.bf7bb9c0.chunk.css"
+  },
+  {
+    "revision": "11f0fc8bf31a709d61f9",
+    "url": "/static/js/2.08d43b85.chunk.js"
+  },
+  {
+    "revision": "9c203323e0ba1bd8f2bc52811e3439b5",
+    "url": "/static/js/2.08d43b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fffe857cbe698e7f30c",
+    "url": "/static/js/main.2c53d70a.chunk.js"
+  },
+  {
+    "revision": "d7dfa028e7551f867043",
+    "url": "/static/js/runtime-main.d0a2d51c.js"
+  },
+  {
+    "revision": "b0a126e4a94424512b3e9fcb746b752d",
+    "url": "/static/media/vpToon.b0a126e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.162caec18d9985c808a9b8d63521bc95.js"
+  "/precache-manifest.2dad832f889c6778a7621ddf4d3cb6c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
