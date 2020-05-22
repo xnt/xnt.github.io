73406176266dce49d0c6315ab0b02ab6d39e0689
Vicente Plata <xientyto@gmail.com>
2020-05-21 22:26:50 -0400

    Updates

---
+++ precache-manifest.a71f248eb44dbb01204b054bd89d9bda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b666c7a2fb54deb65f08b688d6dfc69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96fc132b2b5a8cfedddc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e711f979ae62139d20e",
+    "url": "/static/css/main.9d48ae96.chunk.css"
+  },
+  {
+    "revision": "96fc132b2b5a8cfedddc",
+    "url": "/static/js/2.26616c76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.26616c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e711f979ae62139d20e",
+    "url": "/static/js/main.427fe4df.chunk.js"
+  },
+  {
+    "revision": "6f7c7ea4b414718a8053",
+    "url": "/static/js/runtime-main.1cedac0b.js"
+  },
+  {
+    "revision": "b0a126e4a94424512b3e9fcb746b752d",
+    "url": "/static/media/vpToon.b0a126e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dad832f889c6778a7621ddf4d3cb6c5.js"
+  "/precache-manifest.a71f248eb44dbb01204b054bd89d9bda.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
