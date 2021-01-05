fdc413fe75f437caa62ecd15ff15e083954c61ea
Vicente Plata <xientyto@gmail.com>
2021-01-04 22:35:59 -0500

    Updates

---
+++ precache-manifest.c4c85d244338de17f881a806f9e29a53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884a7dddc159c5010af8a045059c02a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef1ec7fa818e0062f63a",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "2480dbaa321dadcb8e12",
+    "url": "/static/css/main.7ec8ce97.chunk.css"
+  },
+  {
+    "revision": "ef1ec7fa818e0062f63a",
+    "url": "/static/js/2.66d849f9.chunk.js"
+  },
+  {
+    "revision": "aadd261c4fdea152906a949a61e6f4ef",
+    "url": "/static/js/2.66d849f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2480dbaa321dadcb8e12",
+    "url": "/static/js/main.f86ae7b8.chunk.js"
+  },
+  {
+    "revision": "72b4ea2f6ba2a31b7c79",
+    "url": "/static/js/runtime-main.35ded621.js"
+  },
+  {
+    "revision": "b0a126e4a94424512b3e9fcb746b752d",
+    "url": "/static/media/vpToon.b0a126e4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07272ec9d8bd70c3fee3ccdb969a0988.js"
+  "/precache-manifest.c4c85d244338de17f881a806f9e29a53.js"
 );
 
 self.addEventListener('message', (event) => {
